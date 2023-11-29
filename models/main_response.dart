e7dfd0c43efafa8dff3ba8bf7520ca61668c00a6
James Perih <james@clickspace.com>
2023-11-28 20:07:54 -0600

    fixes for default head/tail value.

--- models/main_response.dart
+++ models/main_response.dart
@@ -1,29 +1,46 @@
+import 'enums/heads.dart';
+import 'enums/tails.dart';
+
+/// https://docs.battlesnake.com/api/requests/info
 class MainResponse {
   static const keyApiVersion = 'apiversion';
   static const keyColor = 'color';
   static const keyHead = 'head';
   static const keyTail = 'tail';
-  
+  static const keyAuthor = 'author';
+
   final String apiVersion;
-  final String author;
-  final String primaryColor;
-  final String headColor;
-  final String tailColor;
+  final String? author;
+  final String? color;
+  final Heads? head;
+  final Tails? tail;
 
   MainResponse({
     required this.apiVersion,
-    required this.author,
-    required this.primaryColor,
-    required this.headColor,
-    required this.tailColor,
+    this.author,
+    this.color,
+    this.head,
+    this.tail,
   });
 
   Map<String, dynamic> toJson() {
-    return {
+    final response = {
       keyApiVersion: apiVersion,
-      keyColor: primaryColor,
-      keyHead: headColor,
-      keyTail: tailColor,
     };
+
+    if (color != null) {
+      response[keyColor] = color!;
+    }
+    if (author != null) {
+      response[keyAuthor] = author!;
+    }
+    if (head != null) {
+      response[keyHead] = head!.unsanitizedStringValue;
+    }
+    if (tail != null) {
+      response[keyTail] = tail!.unsanitizedStringValue;
+    }
+
+    return response;
   }
 }
